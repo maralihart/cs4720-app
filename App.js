fbc4c7beb0445652f4a4ce561be79d75bee520a2
iadoro <adgupta05@gmail.com>
2021-09-23 02:48:05 -0400

    set up basic functionality

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from './components/SearchBar/SearchBar';
+import SplashScreen from './components/SplashScreen/SplashScreen';
+// import Login from './components/Login/Login';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <SearchBar/> */}
+      <SplashScreen/>
       <StatusBar style="auto" />
     </View>
   );
--- components/SplashScreen/SplashScreen.js
+++ components/SplashScreen/SplashScreen.js
@@ -1,1 +1,31 @@
 // TODO: Aditi
+
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 300,
+    height: 300,
+  },
+  logo: {
+    width: 500,
+    height: 500,
+  },
+});
+
+const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('./logo.png')}
+      />
+    </View>
+  );
+}
+
+export default DisplayAnImage;
