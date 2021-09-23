43449b41c97d346ec3fb1e2f437cfb239bc02222
iadoro <adgupta05@gmail.com>
2021-09-23 02:48:05 -0400

    set up basic functionality

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Listing from './components/Listing/Listing.js';
-import ListingPreview from './components/ListingPreview/ListingPreview.js'
+import SearchBar from './components/SearchBar/SearchBar';
+import SplashScreen from './components/SplashScreen/SplashScreen';
+// import Login from './components/Login/Login';
+
 
 export default function App() {
-  return(
-    //Uncomment this seciton to test ListingPreview
-    <View>
-      <ListingPreview
-      Title = '[Trade] Bazaar title'
-      Content = 'Information about the item here. What are you looking to trade it for, etc. Labore sunt veniam amet est. Minim nisi dolor eu ad incididunt cillum elit ex ut. Dolore exercitation nulla tempor consequat aliquip occaecat. Nisi id ipsum irure aute. Deserunt sit aute irure quis nulla eu consequat fugiat Lorem sunt magna et consequat labore. Laboris incididunt id Lorem est duis deserunt nisi dolore eiusmod culpa exercitation consectetur.{"\n"}{"\n"}Fugiat do aliqua laboris cillum sint dolor officia adipisicing excepteur fugiat officia. Cupidatat ut elit consequat ea laborum occaecat laborum aute consectetur Lorem exercitation. Lorem anim minim officia aliquip commodo deserunt mollit. Duis deserunt quis cillum voluptate duis ipsum quis incididunt elit excepteur excepteur labore duis cillum. Voluptate sint nulla minim eiusmod in nulla. Ea id ad duis esse adipisicing culpa ex esse ea dolore consequat. Reprehenderit eu minim veniam aliquip do ipsum duis do qui adipisicing aliquip ad occaecat.{"\n"}{"\n"}Enim reprehenderit sunt do do. Ex fugiat nisi sit anim culpa nisi. Non labore fugiat culpa magna. Commodo esse Lorem ex duis do et do. Est laboris cupidatat ad est anim adipisicing sit labore do dolor officia. Fugiat consequat in excepteur reprehenderit id aliquip voluptate sint enim proident aliquip occaecat quis non. Ad nulla aliqua est amet aliqua sint est occaecat amet sunt.{"\n"}{"\n"}Ullamco laborum qui sint officia officia ad Lorem culpa minim voluptate occaecat id duis esse. Ullamco sunt magna consectetur excepteur. Id cupidatat proident ex ad elit laboris. Nulla aute sit occaecat laboris. Laboris minim aliquip exercitation elit commodo ipsum et fugiat sit anim laborum proident cillum laboris.{"\n"}{"\n"}Et cillum tempor esse sit anim et veniam. Tempor magna irure ex ea id fugiat. Ex eu sint tempor quis exercitation amet ut deserunt sit fugiat aliquip nostrud nulla. Eu sunt laborum elit consectetur tempor sunt dolore in sunt reprehenderit voluptate dolore adipisicing. Proident eiusmod reprehenderit quis sint exercitation voluptate non labore. Sunt non sint in Lorem occaecat dolore tempor ipsum et proident sit Lorem ipsum officia.'
-      Header = 'Poster'
-      />
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <SearchBar/> */}
+      <SplashScreen/>
+      <StatusBar style="auto" />
     </View>
 
 //Uncomment this section to test Listing
--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,1 +1,62 @@
 // TODO: Aditi -- search bar + search results (listing preview)
+
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import { X } from 'react-native-feather';
+import { TextInput } from 'react-native-gesture-handler';
+import { Row, Text } from '../Essentials/Essentials';
+
+export default function SearchBar() {
+    const [topic, onSearchTopic] = useState('Search')
+    const [name, onChangeName] = useState(' ')
+    const [resuts, onFindResults] = useState(' ')
+
+    const findResults = () => {
+        // implement results
+    }
+
+    const filter = () => {
+        // implement filter button
+    }
+
+    return (
+        <View style={styles.container}>
+            <Row>
+                <X fill="#000" width={32} height={32} />
+                <Text size={24}>Search</Text>
+            <Button 
+                onPress={() => filter()} 
+                title= "Filter" 
+            />
+            </Row>
+
+        <TextInput 
+            style = {styles.input}
+            onChangeText = {onSearchTopic}
+            value = {topic}
+            placeholder = " Search "
+            />
+        <Button 
+            onPress={() => findResults()} 
+            title= "Go" 
+            />
+        <StatusBar style="auto" />
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
