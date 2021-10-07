e0b19986b2033d84dad6a3395c80fa206b3f13d9
iadoro <adgupta05@gmail.com>
2021-10-07 12:09:36 -0400

    Styled header portion of search bar

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -6,80 +6,122 @@ import { Button, StyleSheet, View } from 'react-native';
 import { X } from 'react-native-feather';
 import { TextInput } from 'react-native-gesture-handler';
 import { Row, Text } from '../Essentials/Essentials';
+import { Listing } from '../Listing/Listing';
 import {Image} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        marginRight: 0,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: "lightgray",
+        padding: 10,
+        width: 300,
+        flex: 4
+    },
+    text: {
+        color: '#db6b5c',
+        fontSize: 12
+    },
+    item: {
+        flex: 1,
+        width: "100%"
+    },
+    smallLogo: {
+        flex: 2,
+        height: 20,
+        resizeMode: 'contain'
+    },
+    searchContainer: {
+        flex: 0.05,
+        flexDirection: "row",
+        alignItems: "center",
+        // justifyContent: "space-evenly",
+        width: "100%"
+    },
+    bannerContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        flex: 0.1,
+        paddingTop: 50,
+        paddingLeft: 30,
+        paddingRight: 30
+    },
+    searchBarContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 300
+    },
+    
+    });
 
 export default function SearchBar() {
     const [topic, onSearchTopic] = useState('Search')
     const [name, onChangeName] = useState(' ')
     const [resuts, onFindResults] = useState(' ')
-
+    
     const findResults = () => {
         // implement results
     }
-
+    
     const filter = () => {
         // implement filter button
     }
-
-    const styles = StyleSheet.create({
-        container: {
-          paddingTop: 10,
-        },
-        tinyLogo: {
-          width: 300,
-          height: 300,
-        },
-        logo: {
-          width: 50,
-          height: 50,
-        },
-      });
     
     return (
         <View style={styles.container}>
-        <Image
-          style={styles.logo}
-        //   source={require('./bazaar.png')}
-          source={require('./bazaar.jpg')}
-
-        />
-        <View style={styles.container}>
-            <Row>
-                <X fill="#000" width={32} height={32} />
-                <Text size={24}>Search</Text>
+            <View style={styles.bannerContainer}>
+                <Text>{'<'}</Text>    
+                <Image style={styles.smallLogo} source={require('./bazaar.jpg')}/>
+                <Text></Text>
+            </View>
+            <View style={styles.searchContainer}>
+                <View style={styles.searchBarContainer}>
+                    <TextInput 
+                        style = {styles.input}
+                        onChangeText = {onSearchTopic}
+                        placeholder = "Search"
+                        />
+                </View>
+                <Button 
+                    onPress={() => filter()} 
+                    style = {styles.text}
+                    title= "Filter" 
+                />
+            </View>              
+            <View style={styles.item}>
+                {/* <Row>
+                    <X fill="#000" width={32} height={32} />
+                    <Text size={24}>Search</Text>
+                <Button 
+                    onPress={() => filter()} 
+                    style = {styles.text}
+                    title= "Filter" 
+                />
+                </Row>
+        
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {onSearchTopic}
+                placeholder = "Search"
+                />
             <Button 
-                onPress={() => filter()} 
-                title= "Filter" 
-            />
-            </Row>
-    
-        <TextInput 
-            style = {styles.input}
-            onChangeText = {onSearchTopic}
-            value = {topic}
-            placeholder = " Search "
-            />
-        <Button 
-            onPress={() => findResults()} 
-            title= "Go" 
-            />
-        <StatusBar style="auto" />
-        </View>
+                onPress={() => findResults()} 
+                title= "Go" 
+                />
+            <StatusBar style="auto" /> */}
+            </View>
         </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
